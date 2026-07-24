fc8e340efc20845903d1f44b3fbdc6ec7fed0466
orangecatprog <luisrguezgam@gmail.com>
2026-07-24 14:50:27 +0200

    chore(initial): configure Prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,13 +5,43 @@ import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  tseslint.configs.recommended,
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+    {
+        ignores: ["dist/**", "coverage/**", "node_modules/**"],
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    },
+    tseslint.configs.recommended,
+    {
+        files: ["**/*.json"],
+        plugins: { json },
+        language: "json/json",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.jsonc"],
+        plugins: { json },
+        language: "json/jsonc",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.json5"],
+        plugins: { json },
+        language: "json/json5",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.md"],
+        plugins: { markdown },
+        language: "markdown/gfm",
+        extends: ["markdown/recommended"],
+    },
+    { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+    eslintConfigPrettier,
 ]);
